555f55fbbb3e077b47504fd0a62d6a53062270c4
Chris Nesbitt-Smith <chris@cns.me.uk>
2019-04-02 00:29:03 +0100

    add a faker tool

---
+++ faker.js
@@ -0,0 +1,19 @@
+const jsf = require("json-schema-faker")
+jsf.extend('faker', () => require('faker'))
+jsf.extend('chance', () => require('chance'))
+
+const batchSize = 1000
+
+const lots = {
+  type: "array",
+  minItems: batchSize,
+  maxItems: batchSize,
+  items: {
+    type: "object",
+    "$ref": "CDPRequestCreate.json",
+  }
+}
+
+jsf.resolve(lots, null, "cdp-composite-schema/src/main/resources/JSONSchema")
+  .then(fakes => console.log(JSON.stringify(fakes)))
+
